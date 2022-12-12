4086f37b2fc75be2c5f87b4db65dac359e26bee7
Guilherme Cardoso <cardosoghr@gmail.com>
2022-12-12 19:11:22 -0300

    adding pokemon list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
 
-const offset = 0
-const limit = 10
-const url = `http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -15,22 +11,16 @@ function convertPokemonToLi(pokemon) {
                     <li class="type">grass</li>
                 </ol>
 
-                <img src"" alt="${pokemon.name}">
+                <img src""
+                    alt="${pokemon.name}">
             </div>
         </li>
     `   
 }
 
+
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
