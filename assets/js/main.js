b0ae1d2bddb05522043175fcab77a6e3aa7e25a5
Guilherme Cardoso <cardosoghr@gmail.com>
2022-12-13 20:23:57 -0300

    finishing the base project

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,19 +10,20 @@ function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
                 <li class="pokemon ${pokemon.type}">
-                    <span class"number">#${pokemon.number}</span>
-                    <span class"name">${pokemon.name}</span>
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
         
                     <div class="detail">
-                        <ol class=types">
+                        <ol class="types">
                             ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                         </ol>
         
-                        <img src"${pokemon.photo}"
+                        <img src="${pokemon.photo}"
                             alt="${pokemon.name}">
                     </div>
                 </li>
-            `).join('')
+            `)
+            .join('')
 
         pokemonList.innerHTML += newHtml
     })
